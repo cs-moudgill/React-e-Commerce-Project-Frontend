d188204f048e4547c565f3d0d1ce4b044675f9af
cs-moudgill <channpreet.moudgill@gmail.com>
2021-01-19 17:36:44 -0400

    login completed

--- src/auth/helper/index.js
+++ src/auth/helper/index.js
@@ -1,8 +1,8 @@
 import {API} from '../../backend'
 
-
+//fetch operation starts here---
 export const signup=(user)=>{
-return fetch(API,{
+return fetch(`${API}/signup`,{
 method:'POST',
 headers:{
     'Content-Type':'application-json'
@@ -16,7 +16,7 @@ body:JSON.stringify(user)
 };
 
 export const signin=(user)=>{
-    return fetch(API,{
+    return fetch(`${API}/signin`,{
     method:'POST',
     headers:{
         'Content-Type':'application/json'
@@ -28,3 +28,34 @@ export const signin=(user)=>{
     })
     .catch(err=>console.log(err));
     };
+
+export const authenticate=(data,next)=>{
+    if(typeof window!=="ündefined"){
+        localStorage.setItem('jwt',JSON.stringify(data));
+        next();
+    }
+}
+
+export const signout=next=>{
+    if(typeof window!=="ündefined"){
+        localStorage.removeItem('jwt');
+        next();
+        return fetch(`${API}/signout`,{   //get back to backend.
+            method:'GET'
+        })
+        .then((response)=>console.log('signout success'))
+        .catch((err)=>console.log(err))
+    }
+    };
+
+    export const isAuthenticated=()=>{ // check to keep signed in.
+        if(typeof window=='undefined'){
+            return false
+        }
+        if(localStorage.getItem('jwt')){
+            return JSON.parse(localStorage.getItem('jwt'))
+        }else{
+            return false;
+        }
+    }
+  
